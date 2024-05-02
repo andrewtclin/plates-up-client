e440d8cd3dcf9cbb2e8982396f23a198fba6517d
TC Lin <chuntcdj@gmail.com>
2024-05-02 17:16:45 +1000

    Adding homepage progress, header

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 
 export default function Home() {
   const navigation = useNavigation();
@@ -12,8 +13,26 @@ export default function Home() {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">Home</Text>
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            sources={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+      </Text>
     </SafeAreaView>
   );
 }
