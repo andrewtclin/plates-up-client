61b8f5a2b542c7cd2571a3cb9c5d42f4d77e59a3
TC Lin <chuntcdj@gmail.com>
2024-05-10 16:59:17 +1000

    Update progress page prototype

--- utils/data.js
+++ utils/data.js
@@ -1,3 +1,6 @@
 export const PRIMARY_BLUE = "#243E62";
+export const PRIMARY_BLUE_RGBA = "rgba(36, 62, 98, 1)";
 export const PRIMARY_BLUE_LIGHT = "#919EB0";
+export const PRIMARY_BLUE_LIGHT_RGBA = "rgba(145, 158, 176, 1)";
 export const PRIMARY_GOLD = "#C69400";
+export const PRIMARY_GOLD_RGBA = "rgba(198, 148, 0, 1)";
